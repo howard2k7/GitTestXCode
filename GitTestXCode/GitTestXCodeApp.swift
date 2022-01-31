caf97a0cc5a424fc36ef91e1f3fe52addc05c7f3
howard2k7 <howard2k7@aim.com>
2022-01-31 16:02:24 +0100

    Initial Commit

---
+++ GitTestXCode/GitTestXCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestXCodeApp.swift
+//  GitTestXCode
+//
+//  Created by Fabian Schäfle on 31.01.22.
+//
+
+import SwiftUI
+
+@main
+struct GitTestXCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
